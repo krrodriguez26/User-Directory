80a6346eb8e6fb45698dcfac5d00b0abd61fafa0
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-05-17 21:08:36 -0400

    Added styling to search feature

---
+++ userdirectory/src/components/search/index.js
@@ -0,0 +1,69 @@
+import React, {Component} from "react";
+import employees from "../../employees.json";
+import "./style.css";
+import EmployeeCard from "../EmployeeCard";
+
+
+
+class Search extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      employees,
+      filteredEmployees: []
+    }
+  }
+  filterList = e => {
+    const filterTarget = e.target.value;
+    let employeeList = this.state.employees;
+    employeeList = employeeList.filter(i =>{
+      let values = Object.values(i)
+        .join("")
+        .toLocaleLowerCase();
+        return values.indexOf(filterTarget.toLowerCase()) !== -1;
+    })
+    this.setState({filteredEmployees: employeeList});
+  }
+  
+  render(){
+    return(
+      <div className="filter-list">
+        <input className="search" type="text" placeholder="Search" onChange={this.filterList}/>
+        
+        {
+          this.state.filteredEmployees[0]?
+          
+          this.state.filteredEmployees.map(employee => (
+            <EmployeeCard
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            position={employee.position}
+            birth={employee.birth}
+            country={employee.country}
+            />
+          )) 
+          :
+          this.state.employees.map(employee => (
+            <EmployeeCard
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            image={employee.image}
+            position={employee.position}
+            birth={employee.birth}
+            country={employee.country}
+            />
+          ))
+
+        
+        }
+        
+  
+      </div>
+    )
+  }
+}
+
+export default Search;
