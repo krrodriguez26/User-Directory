750196a635e23dd55764091ba5d3fef08e2f1e23
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-05-18 01:07:41 -0400

    Added api

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -1,21 +1,65 @@
-import React, { components } from 'react';
-import wrapper from "./components/wrapper";
-import jumbotron from "./components/title";
-import employees from "./employees.json";
-import main from "./components/main";
+import React, {Component} from 'react';
 
-class App extends components {
-  state = {
-    employees
-  };
+class RandomUser extends Component {
+    constructor() {
+        super();
+        this.state = {
+            user: [],
+            isLoading: false,
+            error: null
+        }
+    }
 
-  render() {
-    return (
-      <wrapper>
-        <jumbotron />
-        <main />
-      </wrapper>
-    )
-  }
-};
-export default App;
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        this.fetchRandomUser();        
+    }
+
+    fetchRandomUser = async() => {
+        try {
+            await fetch('https://randomuser.me/api/')
+            .then(results => {
+                return results.json();
+            })
+            .then(data => {
+                let user = data.results.map((user) => {
+                    let userElm = '';
+                    
+                    if (typeof user !== undefined && typeof user.name !== undefined && typeof user.picture != undefined) {
+                        userElm = <div key={user}>
+                            <h2>{user.name.first} {user.name.last}</h2>
+                            <img src={user.picture.large} alt="" />
+                            </div>;
+                    }
+                    
+                    return(userElm)
+                });
+
+                this.setState({user: user, isLoading: false});
+            });
+        }
+        catch(error) {
+            this.setState({ error: error, isLoading: false });
+        }
+    }
+
+    render() {
+        let { user, isLoading, error } = this.state;
+
+        if (error) {
+            return <p>{error.message}</p>;
+          }
+          
+        if (isLoading) {
+            return <p>Loading...</p>;
+          }
+
+        return (
+            <div>
+                {user}
+            </div>
+        )
+    }
+}
+
+export default RandomUser;
--- userdirectory/src/components/search/index.js
+++ userdirectory/src/components/search/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import employees from "../../employees.json";
 import "./style.css";
-import EmployeeCard from "../employeecard";
+import EmployeeCard from "../EmployeCard";
 
 
 
