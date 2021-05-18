d0bb8e6fcadcc85538c6cdf10eeb59530dcf23f9
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-05-18 17:23:52 -0400

    Removed components file

--- userdirectory/src/App.js
+++ userdirectory/src/App.js
@@ -13,4 +13,30 @@ function App() {
   );
 }
 
+renderTableData(); {
+  return this.state.employees.map((employee, index) => {
+     const { id, name, age, email } = employee //destructuring
+     return (
+        <tr key={id}>
+           <td>{id}</td>
+           <td>{name}</td>
+           <td>{age}</td>
+           <td>{email}</td>
+        </tr>
+     )
+  })
+}
+
+render(); {
+  return (
+     <div>
+        <h1 id='title'>React Dynamic Table</h1>
+        <table id='employee'>
+           <tbody>
+              {this.renderTableData()}
+           </tbody>
+        </table>
+     </div>
+  )
+}
 export default App;
