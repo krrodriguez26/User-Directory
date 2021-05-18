738db21ea393e1737f72dd4cd9ee83a689c0b063
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-05-17 21:03:14 -0400

    Added index and syling to employee card

---
+++ userdirectory/src/components/employeecard/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+
+function EmployeeCard(props) {
+  
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+            <div className="content">
+                <ul>
+                <li>
+                  <strong>Name:</strong> {props.name} 
+                </li> 
+                <li>
+                  <strong>Position:</strong> {props.position}
+                </li>
+                <li>
+                  <strong>Date of Birth:</strong> {props.birth}
+                </li>
+                <li>
+                  <strong>Country:</strong> {props.country}
+                </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default EmployeeCard;
